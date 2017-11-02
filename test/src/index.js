816feb0846dce990838e9bcc22f5d81cb1453952
dpliakos <dimitrispl96@gmail.com>
2017-11-02 20:48:11 +0200

    Test

---
+++ test/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <p> lala </p>
+    );
+  }
+}
+
+
+ReactDOM.render(<Test />, root);
