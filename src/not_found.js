6514402e6e26eeaf9dc9fa4f5bcddaaf7f958dfe
dpliakos <dimitrispl96@gmail.com>
2018-01-24 21:43:22 +0200

    add a simple 404 error page

---
+++ src/not_found.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ERROR extends React.Component {
+
+  render() {
+    return(
+      <center>
+        <br/> <br/> <br/>
+        <p> 404! <br/> Page not found! </p>
+
+      </center>
+    );
+  }
+}
